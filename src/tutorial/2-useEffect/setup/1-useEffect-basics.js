422c8432c565b851b1698ca1c7135d97d76fa334
Tina-Lightning <c.atherholt@gmail.com>
2021-01-05 11:03:05 -0500

    started with useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  // you cannot use hooks inside a conditional
+  useEffect(() => {
+    console.log("call useEffect");
+    if (value >= 1) {
+      document.title = `New Messages (${value})`;
+    }
+  });
+  console.log("render component");
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
